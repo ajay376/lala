f35999e19c2396ec2b2e45e03261cb7d56c7aff2
ajay376 <ajayaryadavjava@gmail.com>
2024-02-25 15:27:34 +0530

    first commit

---
+++ ans.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ans = () => {
+  return (
+    <div>
+      <h1>hello sbcdkjhckbdjcbsdhckjsdbcjvvsduycbj,v hj,cbdjSbcjubdmjcbl/ksd hcbzshg bkjdsv jmdsvj</h1>
+    </div>
+  )
+}
+
+export default ans
